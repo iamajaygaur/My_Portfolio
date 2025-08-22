1ed4f18fa95416f52c1188d3127cc0967f5b6595
Ajay Gaur <iamajaygaur@Ajays-MacBook-Air.local>
2025-08-21 22:39:59 -0600

    Updated portfolio content

--- components/work/Data.jsx
+++ components/work/Data.jsx
@@ -1,43 +1,9 @@
 import Project1 from '../../src/assets/Project1.png';
-import Project2 from '../../src/assets/Project2.png';
-import Project3 from '../../src/assets/Project3.png';
-import Project4 from '../../src/assets/Project4.png';
 import Project5 from '../../src/assets/Project5.png';
-import Project6 from '../../src/assets/Project6.png';
-import Project7 from '../../src/assets/Project7.png';
-import Project8 from '../../src/assets/Project8.png';
-import Project9 from '../../src/assets/Project9.png';
-import Project10 from '../../src/assets/Project10.png';
 import Project11 from '../../src/assets/Project11.png';
 import Project12 from '../../src/assets/Project12.png';
 import Project13 from '../../src/assets/Project13.png';
 import Project14 from '../../src/assets/Project14.png';
-import Project15 from '../../src/assets/Project15.png';
-import Project16 from '../../src/assets/Project16.png';
-import Project17 from '../../src/assets/Project17.png';
-import Project18 from '../../src/assets/Project18.png';
-import Project19 from '../../src/assets/Project19.png';
-import Project20 from '../../src/assets/Project20.png';
-import Project21 from '../../src/assets/Project21.png';
-import Project22 from '../../src/assets/Project22.png';
-import Project23 from '../../src/assets/Project23.png';
-import Project24 from '../../src/assets/Project24.png';
-import Project25 from '../../src/assets/Project25.png';
-import Project26 from '../../src/assets/Project26.png';
-import Project27 from '../../src/assets/Project27.png';
-import Project28 from '../../src/assets/Project28.png';
-import Project29 from '../../src/assets/Project29.png';
-import Project30 from '../../src/assets/Project30.png';
-import Project31 from '../../src/assets/Project31.png';
-import Project32 from '../../src/assets/Project32.png';
-import Project33 from '../../src/assets/Project33.png';
-import Project34 from '../../src/assets/Project34.png';
-import Project35 from '../../src/assets/Project35.png';
-import Project36 from '../../src/assets/Project36.png';
-import Project37 from '../../src/assets/Project37.png';
-import Project38 from '../../src/assets/Project38.png';
-import Project from '../../src/assets/Project.png';
-
 export const projectsData = [
 	// API Projects
 	{
